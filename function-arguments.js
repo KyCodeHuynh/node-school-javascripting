23f102d7763787f093387e133c062bf1a5036740
Ky-Cuong Huynh <kycodehuynh@gmail.com>
2016-07-17 17:51:25 -0700

    Complete "Function Arguments"

---
+++ function-arguments.js
@@ -0,0 +1,7 @@
+// Functions can have any number of arguments of any type
+// Returns the result of first + (second * third)
+function math (first, second, third) {
+    return first + (second * third);
+}
+
+console.log(math(53, 61, 67));
